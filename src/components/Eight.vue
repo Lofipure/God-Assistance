d38509e7f9ddb95d90df92d066000958562b505e
Lofipure <littlebanana@126.com>
2020-06-14 10:01:21 +0800

    version 1.4.2

--- src/components/Eight.vue
+++ src/components/Eight.vue
@@ -1,61 +1,64 @@
 <template>
     <div>
         <div class="actions">
-            <v-row justify="center">
-                <v-col v-for="(item,i) in item" v-bind:key="i" cols="12">
-                    <v-card>
-                        <v-card-title>第{{i+1}}回合</v-card-title>
-                        <v-card-text>
-                            <v-row justify="center">
-                                <v-col cols="4">
-                                    <v-text-field label="狼人杀害" type="number" v-model="item.wolfKill"
-                                                  v-on:change="judgeDead(i)"></v-text-field>
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-badge icon="mdi-emoticon-kiss"
-                                             v-bind:color="witchSaveStatus === true ? 'success' : 'red'">
-                                        <v-text-field label="女巫解救" type="number" v-model="item.witchSave"
-                                                      v-on:change="judgeDead(i)"
-                                                      v-bind:disabled="!witchSaveStatus"></v-text-field>
-                                    </v-badge>
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-badge icon="mdi-emoticon-cry"
-                                             v-bind:color="witchKillStatus === true ? 'success' : 'red'">
-                                        <v-text-field label="女巫毒害" type="number" v-model="item.witchKill"
-                                                      v-on:change="judgeDead(i)"
-                                                      v-bind:disabled="!witchKillStatus"></v-text-field>
-                                    </v-badge>
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-badge
-                                            v-bind:color="item.prophetCheck === null ? '':(item.prophetCheck === true ? 'red':'success')"
-                                            v-bind:icon="item.prophetCheck === null ? '' : (item.prophetCheck === true ? 'mdi-shield-alert' : 'mdi-shield-check')">
-                                        <v-text-field label="预言家查验" type="number" v-model="item.prophetCheckTemp"
-                                                      v-on:change="judgeWolf($event)"></v-text-field>
-                                    </v-badge>
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-text-field label="放逐" type="number" v-model="item.peopleKill"
-                                                  v-on:change="judgeDead(i)"></v-text-field>
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-badge v-bind:icon="hunterStatus === true ? '':'mdi-emoticon-cry'"
-                                             v-bind:color="hunterStatus === true ? '':'red'">
-                                        <v-text-field label="猎人杀害" type="number" v-model="item.hunterKill"
-                                                      v-on:change="judgeDead(i)"
-                                                      v-bind:disabled="!hunterStatus"></v-text-field>
-                                    </v-badge>
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-text-field label="当选警长" type="number" v-model="police"
-                                                  v-on:change="policeEmit($event)"></v-text-field>
-                                </v-col>
-                            </v-row>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-            </v-row>
+            <v-expansion-panels popout>
+                <v-expansion-panel v-for="(item,i) in item" v-bind:key="i">
+                    <v-expansion-panel-header>第{{i+1}}回合
+                        <template v-slot:actions>
+                            <v-icon>$expand</v-icon>
+                        </template>
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <v-row justify="center">
+                            <v-col cols="4">
+                                <v-text-field label="狼人杀害" type="number" v-model="item.wolfKill"
+                                              v-on:change="judgeDead(i)"></v-text-field>
+                            </v-col>
+                            <v-col cols="4">
+                                <v-badge icon="mdi-emoticon-kiss"
+                                         v-bind:color="witchSaveStatus === true ? 'success' : 'red'" overlap>
+                                    <v-text-field label="女巫解救" type="number" v-model="item.witchSave"
+                                                  v-on:change="judgeDead(i)"
+                                                  v-bind:disabled="!witchSaveStatus"></v-text-field>
+                                </v-badge>
+                            </v-col>
+                            <v-col cols="4">
+                                <v-badge icon="mdi-emoticon-cry"
+                                         v-bind:color="witchKillStatus === true ? 'success' : 'red'" overlap>
+                                    <v-text-field label="女巫毒害" type="number" v-model="item.witchKill"
+                                                  v-on:change="judgeDead(i)"
+                                                  v-bind:disabled="!witchKillStatus"></v-text-field>
+                                </v-badge>
+                            </v-col>
+                            <v-col cols="4">
+                                <v-badge
+                                        v-bind:color="item.prophetCheck === null ? '':(item.prophetCheck === true ? 'red':'success')"
+                                        v-bind:icon="item.prophetCheck === null ? '' : (item.prophetCheck === true ? 'mdi-shield-alert' : 'mdi-shield-check')"
+                                        overlap>
+                                    <v-text-field label="预言家查验" type="number" v-model="item.prophetCheckTemp"
+                                                  v-on:change="judgeWolf($event)"></v-text-field>
+                                </v-badge>
+                            </v-col>
+                            <v-col cols="4">
+                                <v-text-field label="放逐" type="number" v-model="item.peopleKill"
+                                              v-on:change="judgeDead(i)"></v-text-field>
+                            </v-col>
+                            <v-col cols="4">
+                                <v-badge v-bind:icon="hunterStatus === true ? '':'mdi-emoticon-cry'"
+                                         v-bind:color="hunterStatus === true ? '':'red'" overlap>
+                                    <v-text-field label="猎人杀害" type="number" v-model="item.hunterKill"
+                                                  v-on:change="judgeDead(i)"
+                                                  v-bind:disabled="!hunterStatus"></v-text-field>
+                                </v-badge>
+                            </v-col>
+                            <v-col cols="4">
+                                <v-text-field label="当选警长" type="number" v-model="police"
+                                              v-on:change="policeEmit($event)"></v-text-field>
+                            </v-col>
+                        </v-row>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
             <v-row justify="center">
                 <v-col cols="11">
                     <v-btn width="100%" color="success" v-on:click="addNewItem">天黑请闭眼</v-btn>
@@ -69,9 +72,9 @@
                     <v-row justify="center">
                         <v-col cols="10">
                             <v-badge icon="mdi-shield-star" left v-model="playerPoliceStatus[index]" class="police"
-                                     color="orange">
+                                     color="orange" overlap>
                                 <v-badge v-bind:icon="playerStatus[index] === true ? 'mdi-emoticon':'mdi-emoticon-dead'"
-                                         v-bind:color="playerStatus[index] === true ? 'success' : 'red'">
+                                         v-bind:color="playerStatus[index] === true ? 'success' : 'red'" overlap>
                                     <v-select v-bind:items="roles" v-bind:label="index.toString()"
                                               class="selectRole" v-model="playerRole[index]"></v-select>
                                 </v-badge>
@@ -152,4 +155,7 @@
 </script>
 
 <style scoped>
+    .actions {
+        margin-top: 20px;
+    }
 </style>
